c2039e6f8d913c0c28bdb5b792c8d30b3e0db8a7
rli128 <114562309+rli128@users.noreply.github.com>
2022-11-09 14:56:55 -0800

    hi

--- grade.sh
+++ grade.sh
@@ -5,3 +5,19 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+  echo "You are missing ListExamples.java"
+  echo "Fail"
+  exit
+
+fi
+
+cp TestListExamples student-submission/
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+
+grep test > score.txt
